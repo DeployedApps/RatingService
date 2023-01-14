ca92697a22ecece1da2b04b42f9d4d435c804bc8
- <p28@LAMU0Q7GF7X05W6.uhc.com>
2023-01-14 19:59:14 +0530

    Docs API Server

--- src/main/java/com/jat/rating/RatingServicesApplication.java
+++ src/main/java/com/jat/rating/RatingServicesApplication.java
@@ -11,7 +11,7 @@ import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 "[ratelimiters](!https://resilience4j.readme.io/docs/ratelimiter), [circuitbreakers](!https://resilience4j.readme.io/docs/circuitbreaker)" +
 		"and further to specify by Feign client. It is working on non relational database [Mongo DB](!https://www.mongodb.com/). The api will be being consumed by User Services Api & finally Hotel Services API. It is already registered on [Eurka server], " +
 		"(!https://serviceregitsry-production.up.railway.app/), Spring Cloud API-Gateway, Configuration Server & a Combined Document Server. We are also Implementing protection by [Okta Auth](!https://developer.okta.com/docs/reference/api/authn/), [Spring Security](!https://spring.io/projects/spring-security)  & [JSON Web Based Tokens](!https://www.baeldung.com/spring-security-oauth-jwt). Complete Source Code for all of the services is avaiable at [Source Code](!https://github.com/My-Java-Repos/SpringMicroServicesExample)"))
-@ApiOperation("It is prepared with java 17, spring version 2.7.7 enabled with EUREKA SERVER, CONFIG-SERVER,API-GATWAY, DOCUMENT SERVER & SPING SECURITY with OKTA AUTH")
+// @ApiOperation("It is prepared with java 17, spring version 2.7.7 enabled with EUREKA SERVER, CONFIG-SERVER,API-GATWAY, DOCUMENT SERVER & SPING SECURITY with OKTA AUTH")
 public class RatingServicesApplication {
 
 	public static void main(String[] args) {
