8eda255cd1cc9e6d0aba06042eca60ce2639d1a3
- <p28@LAMU0Q7GF7X05W6.uhc.com>
2023-01-14 19:44:18 +0530

    Docs API Server

--- src/main/java/com/jat/rating/RatingServicesApplication.java
+++ src/main/java/com/jat/rating/RatingServicesApplication.java
@@ -6,7 +6,11 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
 @EnableEurekaClient
-
+@OpenAPIDefinition(info = @Info(title = "***  Rating Microservices   *** \nPlease Choose another spec from dropdown for other services from main Document Server API Gatway", version = "3.0", description = "It contains & create Rating Information from different users to Hotels assigned during their stay. API is powered by salient features of Spring Boot e.g. [resilence4j](!https://resilience4j.readme.io/docs),"+ 
+"[ratelimiters](!https://resilience4j.readme.io/docs/ratelimiter), [circuitbreakers](!https://resilience4j.readme.io/docs/circuitbreaker)" +
+		"and further to specify by Feign client. It is working on non relational database [Mongo DB](!https://www.mongodb.com/). The api will be being consumed by User Services Api & finally Hotel Services API. It is already registered on [Eurka server], " +
+		"(!https://serviceregitsry-production.up.railway.app/), Spring Cloud API-Gateway, Configuration Server & a Combined Document Server. We are also Implementing protection by [Okta Auth](!https://developer.okta.com/docs/reference/api/authn/), [Spring Security](!https://spring.io/projects/spring-security)  & [JSON Web Based Tokens](!https://www.baeldung.com/spring-security-oauth-jwt)"))
+@ApiOperation("It is prepared with java 17, spring version 2.7.7 enabled with EUREKA SERVER, CONFIG-SERVER,API-GATWAY, DOCUMENT SERVER & SPING SECURITY with OKTA AUTH")
 public class RatingServicesApplication {
 
 	public static void main(String[] args) {
