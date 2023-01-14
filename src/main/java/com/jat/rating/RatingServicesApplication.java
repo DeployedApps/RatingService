0ece5b5310de6e8b4c971dd319d9cb658b2afe11
- <p28@LAMU0Q7GF7X05W6.uhc.com>
2023-01-14 20:05:41 +0530

    Docs API Server

--- src/main/java/com/jat/rating/RatingServicesApplication.java
+++ src/main/java/com/jat/rating/RatingServicesApplication.java
@@ -1,5 +1,8 @@
 package com.jat.rating;
 
+import io.swagger.annotations.ApiOperation;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
